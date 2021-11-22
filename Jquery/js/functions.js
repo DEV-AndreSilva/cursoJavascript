041ef4865da3b21d6971e3bbdcb8122b1a7d9b00
Andre Luis <andreluis2608@gmail.com>
2021-11-22 11:34:22 -0300

    comentando codigo functions.js

--- Jquery/js/functions.js
+++ Jquery/js/functions.js
@@ -10,12 +10,18 @@ $(window).on('load', function(){
     alert('Ola mundo');
 });*/
 
+
+//Função chamada no termino de execução da pagina
 $(function(){
 
-    
-    
+    //atraso para chamada do evento    
     setTimeout(function () {
+
+        //Manipulando css dos elementos 
+        //Alterando cor de fundo do elemento de classe artigo1 
         $(".artigo1").css("background-color", "#0AB");
+
+        //Alterando a fonte do paragrafo e cor de fundo dentro do elemento de classe artigo2
         $(".artigo2 p").css("font-size", "20px").css("background-color", 'orange');
     }, 2000);
 
